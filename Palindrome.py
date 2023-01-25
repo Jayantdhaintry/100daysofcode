9387ab9917f580d0f0928267765f578c2344e9f5
DhaintryJayant <8802255605and@gmail.com>
2023-01-25 19:22:24 +0530

    2nd day

---
+++ Palindrome.py
@@ -0,0 +1,15 @@
+def isPalindrome(self, x: int) -> bool:
+    temp=x
+    rev=0
+    while(x>0):
+        dig=x%10
+        rev=rev*10+dig
+        x=x//10
+    if(temp==rev):
+        return True
+    else:
+            return False
+
+
+x=int(input())
+print(isPalindrome("Output",x))
