07e1c0bd65b944222d628791f614de56a011ee9d
Jayantdhaintry <91400556+Jayantdhaintry@users.noreply.github.com>
2023-02-14 20:39:51 +0530

    Day 22 of code

---
+++ ValidParentheses.py
@@ -0,0 +1,14 @@
+def isValid(self, s: str) -> bool:
+        stack = [] 
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif len(stack) == 0 or bracket != pairs[stack.pop()]:
+                return False
+
+        return len(stack) == 0
