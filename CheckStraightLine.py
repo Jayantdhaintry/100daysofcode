ea48160af273c349ae5466098206a11f1d3c6035
Jayantdhaintry <91400556+Jayantdhaintry@users.noreply.github.com>
2023-02-27 19:07:16 +0530

    Day 35 of 100 days of code

---
+++ CheckStraightLine.py
@@ -0,0 +1,7 @@
+def checkStraightLine(self, coordinates: list[list[int]]) -> bool:
+        (x1, y1), (x2, y2) = coordinates[:2]
+        for i in range(2, len(coordinates)):
+            (x, y) = coordinates[i]
+            if((y2 - y1) * (x1 - x) != (y1 - y) * (x2 - x1)):
+                return False
+        return True		
