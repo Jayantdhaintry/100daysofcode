ea77acefe6198e47e4353250d71509aa99dd5273
Jayantdhaintry <91400556+Jayantdhaintry@users.noreply.github.com>
2023-04-24 21:27:54 +0530

    Day 90 of code

---
+++ SortArrayByParity.cpp
@@ -0,0 +1,21 @@
+vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2 == 0)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2 != 0)
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+
+
+    }
